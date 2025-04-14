23d115a81585f007f6249376007a394e76757f9c
yellow-dash <shinobuki3770@gmail.com>
2025-04-14 22:59:02 +0900

    Calculate player archetypes
    use a series of functions to pull beatmap attributes according to mods used, calculate score using own formula, and sum

--- main.py
+++ main.py
@@ -12,24 +12,18 @@ client_secret = os.getenv("client_secret")
 api = Ossapi(client_id, client_secret)
 ranking = api.ranking("osu", "performance").ranking
 
-limit = 10
-
-#print(api.user("mrekk").rank_history.data[0])
+limit = 12
 
 class Player:
     def __init__(self, id):
         self.id = id
         self.username = api.user(id).username
-        self.scores = api.user_scores(self.id, "best", mode="osu", limit=100)
+        self.scores = api.user_scores(self.id, "best", mode="osu", limit=limit)
     
     def DisplayTopPlays(self):
         for i in range(len(self.scores)):
             print(f"{self.scores[i].beatmapset.artist:<40.38} {self.scores[i].beatmapset.title:<40.38} {self.scores[i].pp}")
         
-player0 = Player(7562902)
-#print(player0.id, player0.username)
-#player0.DisplayTopPlays()
-
 def GeneratePlayers(n_players): # generate players from rank 1 to n
     PlayerList = []
     for i in range(n_players):
@@ -39,18 +33,37 @@ def GeneratePlayers(n_players): # generate players from rank 1 to n
 
     return PlayerList
 
-PlayerList = GeneratePlayers(2)
+n_players = 100
 
-PlayerList[2].DisplayTopPlays()
+PlayerList = GeneratePlayers(n_players)
 
-#scores_mrekk = (api.user_scores(ranking[0].user.id, "best", mode="osu", limit=limit))
+def GetPlayerScoreModList(scores):
+    modList = []
+    for score in scores:
+        mods = ""
+        for mod in score.mods:
+            if mod.acronym == "CL":
+                mods += "NM"
+                break
+            mods += mod.acronym
+        modList.append(mods)
+    return modList
 
-'''
-for i in range(limit):
-    print(scores_mrekk[i].pp)
-'''
+def ScoreRating(beatmap_id, mods):
+    beatmap = api.beatmap_attributes(beatmap_id,mods=mods)
+    beatmapScore = (beatmap.attributes.aim_difficulty - beatmap.attributes.speed_difficulty) * beatmap.attributes.star_rating
+    return beatmapScore
 
-"""while True:
-    beatmap_id = input("beatmap id: ")
-    beatmap = api.beatmap_attributes(beatmap_id,mods="DT")
-    print(beatmap.attributes.aim_difficulty, beatmap.attributes.speed_difficulty)"""
+def GetPlayerArchetype(Player):
+    modList = GetPlayerScoreModList(Player.scores)
+    #print(modList)
+    playerScore = 0
+    for i in range(limit):
+        #print(Player.scores[i].beatmap.id)
+        mapScore = ScoreRating(Player.scores[i].beatmap.id, modList[i])
+        playerScore += mapScore
+    return playerScore
+
+for player in PlayerList:
+    playerScore = GetPlayerArchetype(player)
+    print(player.username, playerScore)
